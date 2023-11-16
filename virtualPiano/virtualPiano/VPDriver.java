07a2b4ef97539c971d39e37f19621936dc5381cb
kjohnson093 <89599086+kjohnson093@users.noreply.github.com>
2023-11-16 11:58:55 -0600

    Remove unused imports

--- virtualPiano/virtualPiano/VPDriver.java
+++ virtualPiano/virtualPiano/VPDriver.java
@@ -1,11 +1,6 @@
 package virtualPiano;
 
-import java.awt.Dimension;
-
 import javax.sound.midi.MidiUnavailableException;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 public class VPDriver {
 
