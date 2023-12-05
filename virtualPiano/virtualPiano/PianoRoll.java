d1b0f7b626f776230a91e39ba41502c32e8b73fc
kjohnson093 <89599086+kjohnson093@users.noreply.github.com>
2023-12-04 18:59:20 -0600

    Added GUI

--- virtualPiano/virtualPiano/PianoRoll.java
+++ virtualPiano/virtualPiano/PianoRoll.java
@@ -1,18 +1,27 @@
+/*
+ * Virtual piano GUI and keyboard input listener implementation 
+ * 
+ * @authors Jeremiah Jordan, Keondre Johnson
+ */
 package virtualPiano;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.event.*;
 import javax.sound.midi.*;
 import javax.swing.*;
 
-public class PianoRoll extends JFrame implements KeyListener{
+public class PianoRoll extends JPanel implements KeyListener{
 	private Synthesizer synth = MidiSystem.getSynthesizer(); 
 	// Get array of MidiChannels, all sounds must be generated through a channel
 	private final MidiChannel[] mc = synth.getChannels();
 	// Create instrument object from default sound bank.
 	private Instrument[] instr = synth.getDefaultSoundbank().getInstruments();
-	private JFrame frame; //frame for displaying PianoGUI
-
+	private String blackKeys = "23 567 90 SDF HJ";
+    private String whiteKeys = "QWERTYUIOPZXCVBNM";
+    private String allKeys = "Q2W3ER5T6Y7UI9O0PZSXDCFVBHNJM";
+    private boolean[] keyOn = new boolean[allKeys.length()];
 	public PianoRoll() throws MidiUnavailableException {
 		this.synth.open();  
 
@@ -22,39 +31,57 @@ public class PianoRoll extends JFrame implements KeyListener{
 		// Set Piano Roll focusable to allow keyboard input
 		this.setFocusable(true);
 
-		frame = new JFrame("Virtual Piano");  
-		frame.setPreferredSize(new Dimension(1000,400));
+		setPreferredSize(new Dimension(1000,400));
 		JPanel pane = new JPanel();     
-		JButton button1 = new JButton("Click me!");            
-		frame.getContentPane().add(pane);                   
-		pane.add(button1);                                     
-		frame.pack();                                       
-		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-		frame.show();
-		frame.setFocusable(true); //set frame focusable to allow keyboard input
-		frame.addKeyListener(this); //Add key listener to frame to current piano roll
-
-		//Button Example
-		button1.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				mc[5].noteOn(60,600);
-				mc[5].noteOn(64,600);
-				mc[5].noteOn(67,600);
-			}
-		});
-
-
+		JButton button1 = new JButton("Click me!");              
+		pane.add(button1);    
+		setFocusable(true); //set frame focusable to allow keyboard input
+		addKeyListener(this); //Add key listener to frame to current piano roll
 	}
+	@Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+        g.translate(50, 50);
+        
 
+        // draw white keys
+        final int WHITE_KEY_WIDTH = 50;
+        final int WHITE_KEY_HEIGHT = 100;
+        for (int k = 0; k < whiteKeys.length(); k++) {
+            g.setColor(keyOn[allKeys.indexOf(whiteKeys.charAt(k))] ? Color.GREEN
+ : Color.WHITE);
+            g.fillRect(k * WHITE_KEY_WIDTH, 0, WHITE_KEY_WIDTH, WHITE_KEY_HEIGHT);
+            g.setColor(Color.BLACK);
+            g.drawRect(k * WHITE_KEY_WIDTH, 0, WHITE_KEY_WIDTH, WHITE_KEY_HEIGHT);
+            g.drawString(" " + whiteKeys.charAt(k), k * WHITE_KEY_WIDTH + 10, WHITE_KEY_HEIGHT - 10);
+        }
+
+        // draw black keys
+        final int BLACK_KEY_WIDTH = WHITE_KEY_WIDTH / 2;
+        final int BLACK_KEY_HEIGHT = WHITE_KEY_HEIGHT / 2;
+        for (int k = 0; k < blackKeys.length(); k++) {
+            if (blackKeys.charAt(k) == ' ') {
+                continue;
+            }
+            int x = (k + 1) * WHITE_KEY_WIDTH - BLACK_KEY_WIDTH / 2;
+            g.setColor(keyOn[allKeys.indexOf(blackKeys.charAt(k))] ? Color.GREEN
+ : Color.BLACK);
+            g.fillRect(x, 1, BLACK_KEY_WIDTH, BLACK_KEY_HEIGHT);
+            g.setColor(Color.WHITE);
+            g.drawRect(x, 1, BLACK_KEY_WIDTH, BLACK_KEY_HEIGHT);
+            g.drawString(" " + blackKeys.charAt(k), x + 2, BLACK_KEY_HEIGHT - 5);
+        }
+    }
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 
 
 	}
-	
-	/* Primary method for handling keyboard input and converting it to MIDI output.
+
+	/* 
+	 * Primary method for handling keyboard input and converting it to MIDI output.
 	 * The virtual piano is 28 keys from C3 to E5 following a traditional B&W piano 
 	 * pattern. The white keys are from Q-P and Z-M, and the black keys are the 
 	 * number/letter keys above and between the corresponding white keys.
@@ -185,11 +212,131 @@ public class PianoRoll extends JFrame implements KeyListener{
 			mc[5].noteOn(76,600);
 		}
 
-
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
+		int keyCode = e.getKeyCode();		
+		//Plays C3 if 'Q' pressed
+		if (keyCode == KeyEvent.VK_Q) {
+			mc[5].noteOff(48,300);
 
-	}        
+		}
+		//C#3, '2'
+		if (keyCode == KeyEvent.VK_2) {
+			mc[5].noteOff(49,600);
+		}
+		//D3, 'W'
+		if (keyCode == KeyEvent.VK_W) {
+			mc[5].noteOff(50,600);
+		}
+		//D#3, '3'
+		if (keyCode == KeyEvent.VK_3) {
+			mc[5].noteOff(51,600);
+		}
+		//E3, 'E'
+		if (keyCode == KeyEvent.VK_E) {
+			mc[5].noteOff(52,600);
+		}
+
+		//F3, 'R'
+		if (keyCode == KeyEvent.VK_R) {
+			mc[5].noteOff(53,600);
+		}
+		//F#3, '5'
+		if (keyCode == KeyEvent.VK_5) {
+			mc[5].noteOff(54,600);
+		}
+		//G3, 'T'
+		if (keyCode == KeyEvent.VK_T) {
+			mc[5].noteOff(55,600);
+		}
+		//G#3, '6'
+		if (keyCode == KeyEvent.VK_6) {
+			mc[5].noteOff(56,600);
+		}
+		//A3, 'Y'
+		if (keyCode == KeyEvent.VK_Y) {
+			mc[5].noteOff(57,600);
+		}
+		//A#3, '7'
+		if (keyCode == KeyEvent.VK_7) {
+			mc[5].noteOff(58,600);
+		}
+		//B3, 'U'
+		if (keyCode == KeyEvent.VK_U) {
+			mc[5].noteOff(59,600);
+		}
+
+		//C4, 'I'
+		if (keyCode == KeyEvent.VK_I) {
+			mc[5].noteOff(60,600);
+		}
+		//C#4, '9'
+		if (keyCode == KeyEvent.VK_9) {
+			mc[5].noteOff(61,600);
+		}
+		//D4, 'O'
+		if (keyCode == KeyEvent.VK_O) {
+			mc[5].noteOff(62,600);
+		}
+		//D#4, '0'
+		if (keyCode == KeyEvent.VK_0) {
+			mc[5].noteOff(63,600);
+		}
+		//E4, 'P'
+		if (keyCode == KeyEvent.VK_P) {
+			mc[5].noteOff(64,600);
+		}
+
+		//F4, 'Z'
+		if (keyCode == KeyEvent.VK_Z) {
+			mc[5].noteOff(65,600);
+		}
+		//F#4, 'S'
+		if (keyCode == KeyEvent.VK_S) {
+			mc[5].noteOff(66,600);
+		}
+		//G4, 'X'
+		if (keyCode == KeyEvent.VK_X) {
+			mc[5].noteOff(67,600);
+		}
+		//G#4, 'D'
+		if (keyCode == KeyEvent.VK_D) {
+			mc[5].noteOff(68,600);
+		}
+		//A4, 'C'
+		if (keyCode == KeyEvent.VK_C) {
+			mc[5].noteOff(69,600);
+		}
+		//A#4, 'F'
+		if (keyCode == KeyEvent.VK_F) {
+			mc[5].noteOff(70,600);
+		}
+		//B4, 'V'
+		if (keyCode == KeyEvent.VK_V) {
+			mc[5].noteOff(71,600);
+		}
+
+		//C5, 'B'
+		if (keyCode == KeyEvent.VK_B) {
+			mc[5].noteOff(72,600);
+		}
+		//C#5, 'H'
+		if (keyCode == KeyEvent.VK_H) {
+			mc[5].noteOff(73,600);
+		}
+		//D5, 'N'
+		if (keyCode == KeyEvent.VK_N) {
+			mc[5].noteOff(74,600);
+		}
+		//D#5, 'J'
+		if (keyCode == KeyEvent.VK_J) {
+			mc[5].noteOff(75,600);
+		}
+		//E5, 'M'
+		if (keyCode == KeyEvent.VK_M) {
+			mc[5].noteOff(76,600);
+		}
+	}   
 }
--- virtualPiano/virtualPiano/VPDriver.java
+++ virtualPiano/virtualPiano/VPDriver.java
@@ -1,11 +1,24 @@
+/*
+ * Main driver class for virtual piano
+ * 
+ * @authors Jeremiah Jordan, Keondre Johnson
+ */
 package virtualPiano;
 
 import javax.sound.midi.MidiUnavailableException;
+import javax.swing.JFrame;
 
 public class VPDriver {
 
 	public static void main(String[] args) throws MidiUnavailableException {
-		// TODO Auto-generated method stub
+		
 		PianoRoll piano = new PianoRoll();
+		JFrame frame = new JFrame();
+        frame.setSize(970, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.getContentPane().add(piano);
+        frame.setVisible(true);
+        piano.requestFocus();
 	}
 }
